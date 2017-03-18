57b03b374a32de08231a91c492126500b4ccabd2
LamNguyen <lamnh@lungvang.vn>
2017-03-18 12:51:46 +0700

    Update db

--- app/db.js
+++ app/db.js
@@ -1,4 +1,4 @@
 const level = require('level');
 const path = require('path');
-const db = path.resolve(__dirname, '../db', "./db");
+const db = path.resolve(__dirname, '../', "./db");
 module.exports = level(db);
