2c335b2d96ff839444f35a4aededf54dd5817996
LamNguyen <lamnh@lungvang.vn>
2017-03-18 11:59:31 +0700

    Update port docker

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('babel-register');
 require('babel-polyfill');
 const server = require('./app/app');
 
-server.listen(3000, "127.0.0.1",(err, res) => {
-    return console.log(`Your App Running At: http://localhost:${3000}`)
+server.listen(8080, "127.0.0.1",(err, res) => {
+    return console.log(`Your App Running At: http://localhost:${8080}`)
 });
