e744d19050de3eebbb9478ca09ce7b44948c854c
LamNguyen <lamnh@lungvang.vn>
2017-03-18 13:02:07 +0700

    Update Dockerfile

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('babel-register');
 require('babel-polyfill');
 const server = require('./app/app');
 
-server.listen(8080, "127.0.0.1",(err, res) => {
+server.listen(8080, (err, res) => {
     return console.log(`Your App Running At: http://localhost:${8080}`)
 });
